c777b3583a9f7336902b89ce998c2c26c643be26
Cyrill Schumacher <cyrill@schumacher.fm>
2014-05-17 10:02:02 +1000

    REFACTOR: select for disabled or not visible products

--- src/app/code/community/SchumacherFM/FastIndexer/Model/Resource/Catalog/Url.php
+++ src/app/code/community/SchumacherFM/FastIndexer/Model/Resource/Catalog/Url.php
@@ -66,7 +66,13 @@ class SchumacherFM_FastIndexer_Model_Resource_Catalog_Url extends Mage_Catalog_M
     {
         $storeId = (int)$storeId;
 
-        if (false === $this->_getHelper()->excludeDisabledProducts($storeId) && false === $this->_getHelper()->excludeNotVisibleProducts($storeId)) {
+        if (false === $this->_getHelper()->isEnabled() ||
+            (
+                false === $this->_getHelper()->excludeDisabledProducts($storeId) &&
+                false === $this->_getHelper()->excludeNotVisibleProducts($storeId) &&
+                false === $this->_getHelper()->excludeDisableCategories($storeId)
+            )
+        ) {
             return parent::_getProducts($productIds, $storeId, $entityId, $lastEntityId);
         }
 
@@ -83,6 +89,7 @@ class SchumacherFM_FastIndexer_Model_Resource_Catalog_Url extends Mage_Catalog_M
             'website_id' => (int)$websiteId,
             'entity_id'  => (int)$entityId,
         );
+
         /** @var Varien_Db_Select $select */
         $select = $adapter->select()
             //->useStraightJoin(true)
@@ -97,58 +104,22 @@ class SchumacherFM_FastIndexer_Model_Resource_Catalog_Url extends Mage_Catalog_M
             ->limit($this->_productLimit);
 
         // <fastindexer>
-        $_attributes = array();
-        if (true === $this->_getHelper()->excludeDisabledProducts($storeId)) {
-            $_attributes['status'] = Mage_Catalog_Model_Product_Status::STATUS_DISABLED;
-        }
-        if (true === $this->_getHelper()->excludeNotVisibleProducts($storeId)) {
-            $_attributes['visibility'] = Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE;
-        }
-
-        foreach ($_attributes as $attributeCode => $disableValue) {
-            $this->_getProductAttribute($attributeCode, -1, $storeId); // init attributes we do not need a product id
-
-            $tableAlias = 'tj' . $attributeCode;
-            $tAttrId    = (int)$this->_productAttributes[$attributeCode]['attribute_id'];
-            $tTypeId    = (int)$this->_productAttributes[$attributeCode]['entity_type_id'];
-
-            if (1 === (int)$this->_productAttributes[$attributeCode]['is_global'] || $storeId == 0) {
-                $joinColumns = array(
-                    $tableAlias . '.entity_type_id = ' . $tTypeId,
-                    $tableAlias . '.store_id = 0',
-                    $tableAlias . '.attribute_id = ' . $tAttrId,
-                    'e.entity_id = ' . $tableAlias . '.entity_id',
-                    $tableAlias . '.value != ' . $disableValue,
-                );
-                $select->join(
-                    array($tableAlias => $this->_productAttributes[$attributeCode]['table']),
-                    implode(' AND ', $joinColumns),
-                    array()
-                );
-            } else {
-                $t1        = 't1' . $tableAlias;
-                $t2        = 't2' . $tableAlias;
-                $valueExpr = $adapter->getCheckSql('IFNULL(' . $t2 . '.value_id,0) > 0', $t2 . '.value', $t1 . '.value');
-                $select
-                    ->join(
-                        array($t1 => $this->_productAttributes[$attributeCode]['table']),
-                        'e.entity_id = ' . $t1 . '.entity_id AND ' . $t1 . '.store_id = 0 AND ' . $t1 . '.attribute_id = ' . $tAttrId,
-                        array()
-                    )
-                    ->joinLeft(
-                        array($t2 => $this->_productAttributes[$attributeCode]['table']),
-                        $t1 . '.entity_id = ' . $t2 . '.entity_id AND ' . $t1 . '.attribute_id = ' . $t2 . '.attribute_id AND ' .
-                        $t2 . '.store_id = ' . $storeId,
-                        array()
-                    );
-                $select->where($valueExpr . ' != ' . $disableValue);
-            }
-        }
+        $this->_addProductStatusToSelect($select, $storeId);
+        $this->_addProductVisibilityToSelect($select, $storeId);
+        $this->_addProductAttributeToSelect($select, 'name', $storeId);
+        $this->_addProductAttributeToSelect($select, 'url_key', $storeId);
+        $this->_addProductAttributeToSelect($select, 'url_path', $storeId);
         /*</fastindexer>*/
 
         if (false === empty($productIds)) {
             $select->where('e.entity_id IN(?)', $productIds);
         }
+
+        Mage::dispatchEvent('fastindexer_get_products_select', array(
+            'model'    => $this,
+            'select'   => $select,
+            'store_id' => $storeId
+        ));
 
         $rowSet = $adapter->fetchAll($select, $bind);
 
@@ -162,32 +133,22 @@ class SchumacherFM_FastIndexer_Model_Resource_Catalog_Url extends Mage_Catalog_M
         }
         unset($rowSet);
 
-        if (count($products) > 0) {
+        if (count($products) > 0 && false === $this->_getHelper()->excludeCategoryPathInProductUrl($storeId)) {
+            $select = $adapter->select()
+                ->from(
+                    $this->getTable('catalog/category_product'),
+                    array('product_id', 'category_id')
+                )
+                ->where('product_id IN(?)', array_keys($products));
 
-            if (false === $this->_getHelper()->excludeCategoryPathInProductUrl($storeId)) {
-                $select = $adapter->select()
-                    ->from(
-                        $this->getTable('catalog/category_product'),
-                        array('product_id', 'category_id')
-                    )
-                    ->where('product_id IN(?)', array_keys($products));
+            $this->_excludeDisabledCategories($select, $storeId); // fastindexer
 
-                $this->_excludeDisabledCategories($select, $storeId);
-
-                $categories = $adapter->fetchAll($select);
-                foreach ($categories as $category) {
-                    $productId     = $category['product_id'];
-                    $categoryIds   = $products[$productId]->getCategoryIds();
-                    $categoryIds[] = $category['category_id'];
-                    $products[$productId]->setCategoryIds($categoryIds);
-                }
-            }
-
-            foreach (array('name', 'url_key', 'url_path') as $attributeCode) {
-                $attributes = $this->_getProductAttribute($attributeCode, array_keys($products), $storeId);
-                foreach ($attributes as $productId => $attributeValue) {
-                    $products[$productId]->setData($attributeCode, $attributeValue);
-                }
+            $categories = $adapter->fetchAll($select);
+            foreach ($categories as $category) {
+                $productId     = $category['product_id'];
+                $categoryIds   = $products[$productId]->getCategoryIds();
+                $categoryIds[] = $category['category_id'];
+                $products[$productId]->setCategoryIds($categoryIds);
             }
         }
 
@@ -195,10 +156,119 @@ class SchumacherFM_FastIndexer_Model_Resource_Catalog_Url extends Mage_Catalog_M
     }
 
     /**
+     * FastIndexer
+     * Keep method private because of the last two args which can be used for SQL injections
+     *
+     * @param Varien_Db_Select $select
+     * @param string           $attributeCode
+     * @param int              $storeId
+     * @param string           $sqlCompare
+     * @param string|int       $attributeValue
+     * @param bool             $addAttributeToColumn
+     */
+    private function _addProductAttributeToSelect(
+        Varien_Db_Select $select,
+        $attributeCode,
+        $storeId,
+        $sqlCompare = null,
+        $attributeValue = null,
+        $addAttributeToColumn = true
+    )
+    {
+        $adapter = $this->_getReadAdapter();
+        /** @var $adapter SchumacherFM_FastIndexer_Model_Db_Adapter_Pdo_Mysql */
+
+        if (!isset($this->_productAttributes[$attributeCode])) {
+            $attribute                                = $this->getProductModel()->getResource()->getAttribute($attributeCode);
+            $this->_productAttributes[$attributeCode] = array(
+                'entity_type_id' => $attribute->getEntityTypeId(),
+                'attribute_id'   => $attribute->getId(),
+                'table'          => $attribute->getBackend()->getTable(),
+                'is_global'      => $attribute->getIsGlobal()
+            );
+            unset($attribute);
+        }
+
+        $tableAlias = 'tj' . $attributeCode;
+        $tAttrId    = (int)$this->_productAttributes[$attributeCode]['attribute_id'];
+        $tTypeId    = (int)$this->_productAttributes[$attributeCode]['entity_type_id'];
+
+        if (1 === (int)$this->_productAttributes[$attributeCode]['is_global'] || $storeId == 0) {
+            $joinColumns = array(
+                $tableAlias . '.entity_type_id = ' . $tTypeId,
+                $tableAlias . '.store_id = 0',
+                $tableAlias . '.attribute_id = ' . $tAttrId,
+                'e.entity_id = ' . $tableAlias . '.entity_id',
+            );
+            if (null !== $sqlCompare && null !== $attributeValue) {
+                $joinColumns[] = $tableAlias . '.value ' . $sqlCompare . ' ' . $attributeValue;
+            }
+
+            $select->join(
+                array($tableAlias => $this->_productAttributes[$attributeCode]['table']),
+                implode(' AND ', $joinColumns),
+                array()
+            );
+            if (true === $addAttributeToColumn) {
+                $select->columns(array($attributeCode => $tableAlias . '.value'));
+            }
+        } else {
+            $t1        = 't1' . $tableAlias;
+            $t2        = 't2' . $tableAlias;
+            $valueExpr = $adapter->getCheckSql('IFNULL(' . $t2 . '.value_id,0) > 0', $t2 . '.value', $t1 . '.value');
+            $select
+                ->join(
+                    array($t1 => $this->_productAttributes[$attributeCode]['table']),
+                    'e.entity_id = ' . $t1 . '.entity_id AND ' . $t1 . '.store_id = 0 AND ' . $t1 . '.attribute_id = ' . $tAttrId,
+                    array()
+                )
+                ->joinLeft(
+                    array($t2 => $this->_productAttributes[$attributeCode]['table']),
+                    $t1 . '.entity_id = ' . $t2 . '.entity_id AND ' . $t1 . '.attribute_id = ' . $t2 . '.attribute_id AND ' .
+                    $t2 . '.store_id = ' . $storeId,
+                    array()
+                );
+            if (true === $addAttributeToColumn) {
+                $select->columns(array($attributeCode => $valueExpr));
+            }
+
+            if (null !== $sqlCompare && null !== $attributeValue) {
+                $select->where($valueExpr . ' ' . $sqlCompare . ' ' . $attributeValue);
+            }
+        }
+    }
+
+    /**
+     * FastIndexer
+     *
+     * @param Varien_Db_Select $select
+     * @param     int          $storeId
+     */
+    private function _addProductStatusToSelect(Varien_Db_Select $select, $storeId)
+    {
+        if (true === $this->_getHelper()->excludeDisabledProducts($storeId)) {
+            $this->_addProductAttributeToSelect($select, 'status', $storeId, '!=', Mage_Catalog_Model_Product_Status::STATUS_DISABLED, false);
+        }
+    }
+
+    /**
+     * FastIndexer
+     *
+     * @param Varien_Db_Select $select
+     * @param  int             $storeId
+     */
+    private function _addProductVisibilityToSelect(Varien_Db_Select $select, $storeId)
+    {
+        if (true === $this->_getHelper()->excludeNotVisibleProducts($storeId)) {
+            $this->_addProductAttributeToSelect($select, 'visibility', $storeId, '!=', Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE, false);
+        }
+    }
+
+    /**
      * fastIndexer method
      *
      * @param Varien_Db_Select $select
-     * @param                  $storeId
+     * @param int              $storeId
      *
      * @return $this
      */
@@ -208,7 +278,19 @@ class SchumacherFM_FastIndexer_Model_Resource_Catalog_Url extends Mage_Catalog_M
             return $this;
         }
         $attributeCode = 'is_active';
-        $this->_getCategoryAttribute($attributeCode, -1, $storeId); // init attribute
+
+        if (!isset($this->_categoryAttributes[$attributeCode])) {
+            $attribute = $this->getCategoryModel()->getResource()->getAttribute($attributeCode);
+
+            $this->_categoryAttributes[$attributeCode] = array(
+                'entity_type_id' => $attribute->getEntityTypeId(),
+                'attribute_id'   => $attribute->getId(),
+                'table'          => $attribute->getBackend()->getTable(),
+                'is_global'      => $attribute->getIsGlobal(),
+                'is_static'      => $attribute->isStatic()
+            );
+            unset($attribute);
+        }
 
         $tableAlias   = 'tjIsActive';
         $tAttrId      = (int)$this->_categoryAttributes[$attributeCode]['attribute_id'];
@@ -232,7 +314,6 @@ class SchumacherFM_FastIndexer_Model_Resource_Catalog_Url extends Mage_Catalog_M
             );
         $select->where($valueExpr . ' != ' . $disableValue);
         // </@todo remove duplicate code>
-
         return $this;
     }
 
